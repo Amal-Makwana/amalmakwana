3bd860f563e67b73aef164b04de7bc613605dee8
Amal-Makwana <amal.makwana@gmail.com>
2026-02-11 21:20:37 +0000

    Remove footer name label

--- my-site/app/components/site-footer.tsx
+++ my-site/app/components/site-footer.tsx
@@ -2,7 +2,7 @@ export function SiteFooter() {
   return (
     <footer className="border-t border-black/10 py-8">
       <div className="container-shell text-sm text-black/60">
-        <p>© {new Date().getFullYear()} Your Name. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} All rights reserved.</p>
       </div>
     </footer>
   );
